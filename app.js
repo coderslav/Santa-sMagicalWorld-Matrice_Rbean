31c2e7f0c31a841bb49b4d1f162e7ed7e2b91da8
rkomin <kominslav@gmail.com>
2022-01-05 17:22:27 +0100

    Before Big refactoring

--- app.js
+++ app.js
@@ -5,27 +5,48 @@ const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 
-function handleStringsToNumber(body) {
-    if (typeof body.price === 'string') {
-        body.price = parseFloat(body.price);
-    }
-    if (typeof body.category_id === 'string') {
-        if (body.category_id === 'null') {
-            body.category_id = null;
-        } else {
-            body.category_id = parseInt(body.category_id);
+function handleStringsToNumber(price, category_id) {
+    if (typeof price === 'string') {
+        price = parseFloat(price);
+        if (isNaN(price)) {
+            return false;
         }
     }
+    if (typeof category_id === 'string') {
+        if (category_id === 'null') {
+            category_id = null;
+        } else {
+            category_id = parseInt(category_id);
+            if (isNaN(category_id)) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 app.all('/toys', async function (req, res) {
     if (req.method === 'GET') {
         res.json(await Toy.findAll());
     } else if (req.method === 'POST') {
-        if (Object.keys(req.body).length === 4 && ['name', 'description', 'price', 'category_id'].every((el) => Object.keys(req.body).includes(el))) {
-            handleStringsToNumber(req.body);
-            toys = [...toys, req.body];
-            res.json(toys.slice(-1)[0]);
+        if (Object.keys(req.body).length >= 2 && ['name', 'price'].every((el) => Object.keys(req.body).includes(el)) && handleStringsToNumber(req.body.price, req.body.category_id)) {
+            let sameToyCheck = await Toy.findOne({ where: req.body });
+            if (sameToyCheck) {
+                res.status(501).send(`Sorry, but the same toy already exists in the database: ${JSON.stringify(sameToyCheck)}`);
+                return;
+            }
+            try {
+                res.json(await Toy.create({ name: req.body.name, description: req.body.description ? req.body.description : null, price: req.body.price, category_id: req.body.category_id ? req.body.category_id : null }));
+            } catch (error) {
+                switch (error.name) {
+                    case 'SequelizeForeignKeyConstraintError':
+                        res.status(501).send(`Key (category_id)=(${req.body.category_id}) is not present in table "categories".`);
+                        return;
+                    default:
+                        res.json(error);
+                        return;
+                }
+            }
         } else {
             res.sendStatus(422);
         }
@@ -34,29 +55,45 @@ app.all('/toys', async function (req, res) {
     }
 });
 app.all('/toys/:id', async function (req, res) {
-    if (req.params.id >= 1 && req.params.id <= (await Toy.findAll()).length) {
+    let selectedToy = await Toy.findByPk(req.params.id);
+    if (selectedToy) {
         if (req.method === 'GET') {
-            res.json(await Toy.findByPk(req.params.id));
+            res.json(selectedToy);
         } else if (req.method === 'PUT') {
-            if (req.body.category_id || req.body.price) {
-                handleStringsToNumber(req.body);
-                if (req.body.category_id || req.body.category_id === null) {
-                    toys[req.params.id].category_id = req.body.category_id;
-                }
-                if (req.body.price) {
-                    toys[req.params.id].price = req.body.price;
+            if (req.body.price || req.body.category_id) {
+                if (handleStringsToNumber(req.body.price, req.body.category_id)) {
+                    if (req.body.category_id || req.body.category_id === null) {
+                        try {
+                            await selectedToy.update({ category_id: req.body.category_id });
+                        } catch (error) {
+                            switch (error.name) {
+                                case 'SequelizeForeignKeyConstraintError':
+                                    res.status(501).send(`Key (category_id)=(${req.body.category_id}) is not present in table "categories".`);
+                                    return;
+                                default:
+                                    res.json(error);
+                                    return;
+                            }
+                        }
+                    }
+                    if (req.body.price) {
+                        await selectedToy.update({ price: req.body.price });
+                    }
+                } else {
+                    res.sendStatus(422);
+                    return;
                 }
             }
             if (req.body.name) {
-                toys[req.params.id].name = req.body.name;
+                await selectedToy.update({ name: req.body.name });
             }
             if (req.body.description) {
-                toys[req.params.id].description = req.body.description;
+                await selectedToy.update({ description: req.body.description });
             }
-            res.json(toys[req.params.id]);
+            res.json(selectedToy);
         } else if (req.method === 'DELETE') {
-            let deletedObjectsArray = toys.splice(req.params.id, 1);
-            res.json(deletedObjectsArray[0]);
+            await selectedToy.destroy();
+            res.json(selectedToy);
         } else {
             res.sendStatus(404);
         }
@@ -70,9 +107,12 @@ app.all('/categories', async function (req, res) {
         res.json(await Category.findAll());
     } else if (req.method === 'POST') {
         if (Object.keys(req.body).length === 1 && Object.keys(req.body).includes('name')) {
-            handleStringsToNumber(req.body);
-            categories = [...categories, req.body];
-            res.json(categories.slice(-1)[0]);
+            let sameCategoryCheck = await Category.findOne({ where: req.body });
+            if (sameCategoryCheck) {
+                res.status(501).send(`Sorry, but the same category already exists in the database: ${JSON.stringify(sameCategoryCheck)}`);
+                return;
+            }
+            res.json(await Category.create({ name: req.body.name }));
         } else {
             res.sendStatus(422);
         }
@@ -81,17 +121,28 @@ app.all('/categories', async function (req, res) {
     }
 });
 app.all('/categories/:id', async function (req, res) {
-    if (req.params.id >= 1 && req.params.id <= (await Category.findAll()).length) {
+    let selectedCategory = await Category.findByPk(req.params.id);
+    if (selectedCategory) {
         if (req.method === 'GET') {
-            res.json(await Category.findByPk(req.params.id));
+            res.json(selectedCategory);
         } else if (req.method === 'PUT') {
             if (req.body.name) {
-                categories[req.params.id].name = req.body.name;
+                res.json(await selectedCategory.update({ name: req.body.name }));
             }
-            res.json(categories[req.params.id]);
         } else if (req.method === 'DELETE') {
-            let deletedObjectsArray = categories.splice(req.params.id, 1);
-            res.json(deletedObjectsArray[0]);
+            try {
+                await selectedCategory.destroy();
+                res.json(selectedCategory);
+            } catch (error) {
+                switch (error.name) {
+                    case 'SequelizeForeignKeyConstraintError':
+                        res.status(501).send(`You can't delete this category. "${selectedCategory.name}" is still referenced from table "toys".`);
+                        return;
+                    default:
+                        res.json(error);
+                        return;
+                }
+            }
         } else {
             res.sendStatus(404);
         }
@@ -100,18 +151,20 @@ app.all('/categories/:id', async function (req, res) {
     }
 });
 app.all('/categories/:name/toys', async function (req, res) {
+    let selectedCategories = await Category.findAll();
     let categoryObjectID;
     if (
-        (await Category.findAll()).some((el) => {
+        selectedCategories.some((el) => {
             if (el.name.toLocaleLowerCase() === req.params.name.toLocaleLowerCase()) {
                 categoryObjectID = el.id;
                 return true;
             }
         })
     ) {
+        let selectedToys = await Toy.findAll();
         if (req.method === 'GET') {
             let resultArray = [];
-            (await Toy.findAll()).map((el) => {
+            selectedToys.map((el) => {
                 if (el.category_id === categoryObjectID) {
                     resultArray.push(el);
                 }
